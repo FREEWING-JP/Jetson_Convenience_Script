0275d753ee92fea4bcc74584163fb8c7038e5e46
FREEWING-JP <freewing.jp@gmail.com>
2021-01-30 22:58:11 +0900

    update

--- PyTorch/sub_torchvision.sh
+++ PyTorch/sub_torchvision.sh
@@ -24,6 +24,8 @@ sudo apt-get -y install libjpeg-dev zlib1g-dev
 # see below for version of torchvision to download
 # TV_VERSION=v0.5.0
 TV_VERSION=$1
+# where 0.x.0 is the torchvision version
+export BUILD_VERSION=$1
 git clone --branch $TV_VERSION https://github.com/pytorch/vision torchvision --depth 1
 cd torchvision
 sudo python3 setup.py install
@@ -36,4 +38,22 @@ cd ../
 # Verifying The Installation
 python3 -c "import torchvision; print (torchvision.__version__)"
 # 0.5.0a0+85b8fbf
+
+cat << EOF
+# Verification
+import torch
+print(torch.__version__)
+print('CUDA available: ' + str(torch.cuda.is_available()))
+print('cuDNN version: ' + str(torch.backends.cudnn.version()))
+a = torch.cuda.FloatTensor(2).zero_()
+print('Tensor a = ' + str(a))
+b = torch.randn(2).cuda()
+print('Tensor b = ' + str(b))
+c = a + b
+print('Tensor c = ' + str(c))
+
+# Verification
+import torchvision
+print(torchvision.__version__)
+EOF
 

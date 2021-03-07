ddf12059503206178a6901aedca11f6432a64952
FREEWING-JP <freewing.jp@gmail.com>
2021-03-07 17:27:21 +0900

    up

---
+++ TensorFlow/build_TensorFlow_v2_4_1.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+cd `dirname $0`
+pwd
+SCRIPT_DIR=$(pwd)
+echo $SCRIPT_DIR
+
+# ===
+# ===
+# TensorFlow version
+TF_VERSION=`python3 -c "import tensorflow; print (tensorflow.VERSION)"`
+if [ $? = 0 ]; then
+  echo $TF_VERSION
+  if [[ ${TF_VERSION} =~ ^([0-9]+)\..*$ ]]; then
+    echo ${BASH_REMATCH[1]}
+    if [ ! "${BASH_REMATCH[1]}" = "2" ]; then
+      echo "Already Installed TensorFlow v2.x"
+      exit 0
+    fi
+  fi
+
+  echo "Uninstall TensorFlow"
+  chmod +x uninstall.sh
+  bash ./uninstall.sh
+fi
+
+
+# ===
+# TensorFlow Build from source
+# https://www.tensorflow.org/install/source
+# Install Python and the TensorFlow package dependencies
+sudo apt install python3-dev python3-pip
+
+sudo apt-get install -y build-essential  libssl-dev libbz2-dev libreadline-dev libsqlite3-dev zip unzip nkf
+
+sudo pip3 install -U numpy==1.19.5
+sudo pip3 install -U wheel==0.36.2
+# Successfully installed wheel-0.36.2
+sudo pip3 install -U six==1.15.0
+# Successfully installed six-1.15.0
+sudo pip3 install -U keras_preprocessing --no-deps
+# Successfully installed keras-preprocessing-1.1.2
+
+chmod +x dependencies.sh
+bash ./dependencies.sh
+
+
+cd
+git clone https://github.com/tensorflow/tensorflow.git -b v2.4.1 --depth 1
+cd tensorflow
+
+
+(
+echo   # Please specify the location of python. [Default is /usr/bin/python3]:
+echo   # Do you wish to build TensorFlow with OpenCL SYCL support? [y/N]:
+echo   # Do you wish to build TensorFlow with ROCm support? [y/N]:
+echo Y # Do you wish to build TensorFlow with CUDA support? [y/N]: Y
+echo   # Do you wish to build TensorFlow with TensorRT support? [y/N]:
+echo sm_53,7.2 # Please note that each additional compute capability significantly increases your build time and binary size, and that TensorFlow only supports compute capabilities >= 3.5 [Default is: 3.5,7.0]: sm_53,7.2
+echo   # Do you want to use clang as CUDA compiler? [y/N]:
+echo   # Please specify which gcc should be used by nvcc as the host compiler. [Default is /usr/bin/gcc]:
+echo   # Please specify optimization flags to use during compilation when bazel option "--config=opt" is specified [Default is -Wno-sign-compare]:
+echo   # Would you like to interactively configure ./WORKSPACE for Android builds? [y/N]:
+) | ./configure
+
+
+# if Jetson Nano Need export MAX_JOBS=3 to Reduce Memory usage
+tegra_cip_id=$(cat /sys/module/tegra_fuse/parameters/tegra_chip_id)
+echo $tegra_cip_id
+# Jetson Xavier NX
+if [ $tegra_cip_id = "25" ]; then
+  echo Jetson Xavier NX
+  export MAX_JOBS=2
+fi
+
+# Jetson Nano
+if [ $tegra_cip_id = "33" ]; then
+  echo Jetson Nano
+  export MAX_JOBS=1
+fi
+
+
+time bazel build --jobs $MAX_JOBS \
+ --config=cuda \
+ --config=noaws \
+ --config=nogcp \
+ --config=nohdfs \
+ --config=nonccl \
+ //tensorflow/tools/pip_package:build_pip_package
+
+# TensorFlow 1.x
+# To build an older TensorFlow 1.x package, use the --config=v1 option
+
+# Building TensorFlow from source can use a lot of RAM. If your system is memory-constrained, limit Bazel's RAM usage with: --local_ram_resources=2048
+# --local_ram_resources=2048
+
+
+# Install the package
+# sudo pip3 install ./tmp/tensorflow_pkg/tensorflow-version-tags.whl
+
+
+cd $SCRIPT_DIR
+if [ -e ../bell.sh ]; then
+  chmod +x ../bell.sh
+  bash ../bell.sh
+fi
+
--- TensorFlow/dependencies.sh
+++ TensorFlow/dependencies.sh
@@ -2,6 +2,7 @@
 
 # ===
 # ===
+# Python version 3.6 Requires Python >=3.6
 
 # Install system packages required by TensorFlow
 sudo apt-get update
@@ -9,15 +10,17 @@ sudo apt-get -y install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip
 
 # Install and upgrade pip3
 sudo apt-get -y install python3-pip
-sudo pip3 install -U pip testresources setuptools
+sudo pip3 install -U pip testresources setuptools==54.1.1
 
 # Install Cython
 #  raise OSError('Cython needs to be installed in Python as a module')
 # sudo pip3 install --no-use-wheel --no-cache-dir Cython
 # DEPRECATION: --no-use-wheel is deprecated and will be removed in the future.  Please use --no-binary :all: instead.
 # https://kurozumi.github.io/pip/reference/pip_wheel.html
-sudo pip3 install --no-binary :all: --no-cache-dir Cython
-# Successfully installed Cython-0.29.20
+# sudo pip3 install --no-binary :all: --no-cache-dir Cython
+# Successfully installed Cython-0.29.22
+sudo pip3 install -U Cython==0.29.22
+sudo pip3 install -U numpy==1.19.5
 
 # Install the Python package dependencies
 # sudo pip3 install -U numpy==1.16.1 future==0.17.1 mock==3.0.5 h5py==2.9.0 keras_preprocessing==1.0.5 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
@@ -25,5 +28,5 @@ sudo pip3 install --no-binary :all: --no-cache-dir Cython
 
 # Install the Python package dependencies
 # keras_preprocessing==1.1.0
-sudo pip3 install -U numpy==1.16.1 future==0.17.1 mock==3.0.5 h5py==2.9.0 keras_preprocessing==1.1.0 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
+sudo pip3 install -U future==0.18.2 mock==3.0.5 h5py==2.10.0 keras_preprocessing==1.1.2 keras_applications==1.0.8 gast==0.4.0 futures protobuf pybind11
 
